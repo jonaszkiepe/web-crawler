b2e252316bd879e324bf2381d651c75f4244c6ef
jonaszkiepe <loafofbread63@gmail.com>
2024-07-26 21:33:10 +0200

    function to crawl one website

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("Hello World")
+import { argv } from "node:process";
+import { crawlPage } from "./crawl.js";
+
+function main() {
+    if (argv.length < 3) { 
+        console.log("url argument is needed");
+        return;
+    } else if (argv.length > 3) { 
+        console.log("too many arguments");
+        return;
+    };
+    const baseURL = argv[2]
+    crawlPage(baseURL)
+
+}
+
+main()
