e61b93ef8f319256f5892e1b4572ff8ca6a80b71
jonaszkiepe <loafofbread63@gmail.com>
2024-07-26 20:24:50 +0200

    function to get urls from html using jsdom

--- crawl.test.js
+++ crawl.test.js
@@ -1,9 +1,16 @@
 import { test, expect } from "@jest/globals";
-import { normalizeUrl } from "./crawl.js";
+import { normalizeUrl, getURLsFromHTML } from "./crawl.js";
 
 const url = "http://testing.com/";
 const normalizedUrl = "testing.com";
+const htmlBody = '<a href="https://boot.dev">Learn Backend Development</a> <a href="/darwin">Learn Backend Development</a> ';
+const baseURL = "https://boot.dev";
+const result = ["https://boot.dev/"];
 
 test("normalizes an url", () => {
     expect(normalizeUrl(url)).toBe(normalizedUrl);
+});
+    
+test("fishes urls", () => {
+    expect(getURLsFromHTML(htmlBody, baseURL)).toStrictEqual(result);
 });
