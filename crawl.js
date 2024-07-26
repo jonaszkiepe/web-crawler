b53750854f666aeb9b6ebea27b86ffc2bc140aa5
jonaszkiepe <loafofbread63@gmail.com>
2024-07-26 20:24:50 +0200

    function to get urls from html using jsdom

--- crawl.js
+++ crawl.js
@@ -1,14 +1,28 @@
-function normalizeUrl(url) {
-    try { 
-        new URL(url)
-    } catch (error) { 
-        console.log(error)
-        return
+import { JSDOM } from "jsdom";
+
+
+const normalizeUrl = (url) => {
+    try { new URL(url) } catch (error) { 
+        console.log(error);
+        return;
     }
-    const urlObj = new URL(url)
-    let normalizedUrl = urlObj.hostname
-    if (urlObj.pathname !== "/") { normalizedUrl += urlObj.pathname }
-    return normalizedUrl
+    const urlObj = new URL(url);
+    let normalizedUrl = urlObj.hostname + urlObj.pathname;
+    if (normalizedUrl.slice(-1) === "/") { 
+        normalizedUrl = normalizedUrl.slice(0, -1);
+    };
+    return normalizedUrl;
 };
 
-export { normalizeUrl };
+
+const getURLsFromHTML = (htmlBody, baseURL) => {
+    const dom = new JSDOM(htmlBody, { url: baseURL });
+    const urls = [];
+    dom.window.document.querySelectorAll('a').forEach(link => {
+        urls.push(link.href);
+    });
+    return urls;
+};
+
+
+export { normalizeUrl, getURLsFromHTML };
